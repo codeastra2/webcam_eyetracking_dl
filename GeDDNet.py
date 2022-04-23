62e8b4c73feea1741858907464a3f4c9493d255d
codeastra2 <hotshotsrini@hotmail.com>
2022-04-23 23:07:43 +0200

    Added GeDDNet v2

--- GeDDNet.py
+++ GeDDNet.py
@@ -7,12 +7,6 @@ import numpy as np
 
 
 
-
-vgg_wei = np.load("vgg16_weights.npz", allow_pickle=True)
-
-
-
-
 class GEDDnet(nn.Module):
 
     def __init__(self,
@@ -24,60 +18,83 @@ class GEDDnet(nn.Module):
 
         super(GEDDnet, self).__init__()
 
+        self.vgg_wei = np.load("vgg16_weights.npz", allow_pickle=True)
 
         self.num_comb = [0, 256]
         self.num_comb[0] = num_face[-1] + 2*num_eye[-1]
 
         self.max_pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.dropout = nn.Dropout(p=0.5)
 
         self.face_conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv1_1.weight.data  = torch.from_numpy(self.vgg_wei['conv1_1_W'].transpose(3, 2, 0, 1))
+        self.face_conv1_1.bias.data = torch.from_numpy(self.vgg_wei['conv1_1_b'])
+
         self.face_conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv1_2.weight.data = torch.from_numpy(self.vgg_wei['conv1_2_W'].transpose(3, 2, 0, 1))
+        self.face_conv1_2.bias.data = torch.from_numpy(self.vgg_wei['conv1_2_b'])
 
         self.face_conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv2_1.weight.data = torch.from_numpy(self.vgg_wei['conv2_1_W'].transpose(3, 2, 0, 1))
+        self.face_conv2_1.bias.data = torch.from_numpy(self.vgg_wei['conv2_1_b'])
+
         self.face_conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv2_2.weight.data = torch.from_numpy(self.vgg_wei['conv2_2_W'].transpose(3, 2, 0, 1))
+        self.face_conv2_2.bias.data = torch.from_numpy(self.vgg_wei['conv2_2_b'])
 
         self.face_conv2_3 = nn.Conv2d(in_channels=num_face[1], out_channels=num_face[2], kernel_size=(1, 1), stride=(1, 1))
-        
+        self.face_conv2_3_norm = nn.BatchNorm2d(num_face[2])
+
         self.face_conv3_1 = nn.Conv2d(in_channels=num_face[2], out_channels=num_face[3], dilation=rf[0], kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv3_1_norm = nn.BatchNorm2d(num_face[3])
+
         self.face_conv3_2 = nn.Conv2d(in_channels=num_face[3], out_channels=num_face[3], dilation=rf[1],  kernel_size=(3, 3), stride=(1, 1))
+        self.face_conv3_2_norm = nn.BatchNorm2d(num_face[3])
 
         self.face_conv4_1 = nn.Conv2d(in_channels=num_face[3], out_channels=num_face[4], dilation=rf[2], kernel_size=(3, 3),stride=(1, 1))
+        self.face_conv4_1_norm = nn.BatchNorm2d(num_face[4])
+
         self.face_conv4_2 = nn.Conv2d(in_channels=num_face[4], out_channels=num_face[4], dilation=rf[3], kernel_size=(3, 3),stride=(1, 1))
+        self.face_conv4_2_norm = nn.BatchNorm2d(num_face[4])
 
         self.face_fc1 = nn.Linear(12*12*num_face[4], num_face[5])
+        self.face_fc1_norm = nn.BatchNorm1d(num_face[5])
+
         self.face_fc2 = nn.Linear(num_face[5], num_face[6])
+        self.face_fc2_norm = nn.BatchNorm1d(num_face[6])
 
-        self.leye_conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
-        self.leye_conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv1_1.weight.data  = torch.from_numpy(self.vgg_wei['conv1_1_W'].transpose(3, 2, 0, 1))
+        self.eye_conv1_1.bias.data = torch.from_numpy(self.vgg_wei['conv1_1_b'])
+   
+        self.eye_conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv1_2.weight.data  = torch.from_numpy(self.vgg_wei['conv1_2_W'].transpose(3, 2, 0, 1))
+        self.eye_conv1_2.bias.data = torch.from_numpy(self.vgg_wei['conv1_2_b'])
 
-        self.leye_conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
-        self.leye_conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv2_1.weight.data  = torch.from_numpy(self.vgg_wei['conv2_1_W'].transpose(3, 2, 0, 1))
+        self.eye_conv2_1.bias.data = torch.from_numpy(self.vgg_wei['conv2_1_b'])
 
-        self.leye_conv2_3 = nn.Conv2d(in_channels=num_eye[1], out_channels=num_eye[2], kernel_size=(1, 1), stride=(1, 1))
+        self.eye_conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv2_2.weight.data  = torch.from_numpy(self.vgg_wei['conv2_2_W'].transpose(3, 2, 0, 1))
+        self.eye_conv2_2.bias.data = torch.from_numpy(self.vgg_wei['conv2_2_b'])
+
+        self.eye_conv2_3 = nn.Conv2d(in_channels=num_eye[1], out_channels=num_eye[2], kernel_size=(1, 1), stride=(1, 1))
+        self.eye_conv2_3_norm = nn.BatchNorm2d(num_eye[2])
+
+        self.eye_conv3_1 = nn.Conv2d(in_channels=num_eye[2], out_channels=num_eye[3], dilation=r[0], kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv3_1_norm = nn.BatchNorm2d(num_eye[3])
         
-        self.leye_conv3_1 = nn.Conv2d(in_channels=num_eye[2], out_channels=num_eye[3], dilation=r[0], kernel_size=(3, 3), stride=(1, 1))
-        self.leye_conv3_2 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[3], dilation=r[1],  kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv3_2 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[3], dilation=r[1],  kernel_size=(3, 3), stride=(1, 1))
+        self.eye_conv3_2_norm = nn.BatchNorm2d(num_eye[3])
 
-        self.leye_conv4_1 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[4], dilation=r[2], kernel_size=(3, 3),stride=(1, 1))
-        self.leye_conv4_2 = nn.Conv2d(in_channels=num_eye[4], out_channels=num_eye[4], dilation=r[3], kernel_size=(2, 2),stride=(1, 1))
+        self.eye_conv4_1 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[4], dilation=r[2], kernel_size=(3, 3),stride=(1, 1))
+        self.eye_conv4_1_norm = nn.BatchNorm2d(num_eye[4])
 
-        self.leye_fc1 = nn.Linear(31*3*num_eye[4], num_eye[5])
+        self.eye_conv4_2 = nn.Conv2d(in_channels=num_eye[4], out_channels=num_eye[4], dilation=r[3], kernel_size=(2, 2),stride=(1, 1))
+        self.eye_conv4_2_norm = nn.BatchNorm2d(num_eye[4])
 
-        self.reye_conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
-        self.reye_conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
-
-        self.reye_conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
-        self.reye_conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1))
-
-        self.reye_conv2_3 = nn.Conv2d(in_channels=num_eye[1], out_channels=num_eye[2], kernel_size=(1, 1), stride=(1, 1))
-        
-        self.reye_conv3_1 = nn.Conv2d(in_channels=num_eye[2], out_channels=num_eye[3], dilation=r[0], kernel_size=(3, 3), stride=(1, 1))
-        self.reye_conv3_2 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[3], dilation=r[1],  kernel_size=(3, 3), stride=(1, 1))
-
-        self.reye_conv4_1 = nn.Conv2d(in_channels=num_eye[3], out_channels=num_eye[4], dilation=r[2], kernel_size=(3, 3),stride=(1, 1))
-        self.reye_conv4_2 = nn.Conv2d(in_channels=num_eye[4], out_channels=num_eye[4], dilation=r[3], kernel_size=(2, 2),stride=(1, 1))
-
-        self.reye_fc1 = nn.Linear(31*3*num_eye[4], num_eye[5])
+        self.eye_fc1 = nn.Linear(31*3*num_eye[4], num_eye[5])
 
         self.combined_fc1 = nn.Linear(576, num_comb[1])
         self.combined_fc2 = nn.Linear(num_comb[1], 2)
@@ -90,47 +107,81 @@ class GEDDnet(nn.Module):
         X_face = F.relu(self.face_conv2_1(X_face))
         X_face = F.relu(self.face_conv2_2(X_face))
         X_face = F.relu(self.face_conv2_3(X_face))
+        X_face = self.face_conv2_3_norm(X_face)
+        
         X_face = F.relu(self.face_conv3_1(X_face))
+        X_face = self.face_conv3_1_norm(X_face)
+
         X_face = F.relu(self.face_conv3_2(X_face))
+        X_face = self.face_conv3_2_norm(X_face)
         X_face = F.relu(self.face_conv4_1(X_face))
+        X_face = self.face_conv4_1_norm(X_face)
         X_face = F.relu(self.face_conv4_2(X_face))
+        X_face = self.face_conv4_2_norm(X_face)
 
         X_face = F.relu(self.face_fc1(X_face.view(-1)))
-        X_face = F.relu(self.face_fc2(X_face))
 
-        X_leye = F.relu(self.leye_conv1_1(X_leye))
-        X_leye = F.relu(self.leye_conv1_2(X_leye))
+        # Convert to 4D Tensor for batch norm. 
+        # X_face = X_face[None, :]
+        # X_face = self.face_fc1_norm(X_face)
+        X_face = self.dropout(X_face)
+        X_face = F.relu(self.face_fc2(X_face))
+        # X_face = self.face_fc2_norm(X_face)
+
+        X_leye = F.relu(self.eye_conv1_1(X_leye))
+        X_leye = F.relu(self.eye_conv1_2(X_leye))
         X_leye = self.max_pool(X_leye)
 
-        X_leye = F.relu(self.leye_conv2_1(X_leye))
-        X_leye = F.relu(self.leye_conv2_2(X_leye))
-        X_leye = F.relu(self.leye_conv2_3(X_leye))
-        X_leye = F.relu(self.leye_conv3_1(X_leye))
-        X_leye = F.relu(self.leye_conv3_2(X_leye))
-        X_leye = F.relu(self.leye_conv4_1(X_leye))
-        X_leye = F.relu(self.leye_conv4_2(X_leye))
+        X_leye = F.relu(self.eye_conv2_1(X_leye))
+        X_leye = F.relu(self.eye_conv2_2(X_leye))
 
-        X_leye = F.relu(self.leye_fc1(X_leye.view(-1)))
+        X_leye = F.relu(self.eye_conv2_3(X_leye))
+        X_leye = self.eye_conv2_3_norm(X_leye)
 
-        X_reye = F.relu(self.reye_conv1_1(X_reye))
-        X_reye = F.relu(self.reye_conv1_2(X_reye))
+        X_leye = F.relu(self.eye_conv3_1(X_leye))
+        X_leye = self.eye_conv3_1_norm(X_leye)
+
+        X_leye = F.relu(self.eye_conv3_2(X_leye))
+        X_leye = self.eye_conv3_2_norm(X_leye)
+
+        X_leye = F.relu(self.eye_conv4_1(X_leye))
+        X_leye = self.eye_conv4_1_norm(X_leye)
+
+        X_leye = F.relu(self.eye_conv4_2(X_leye))
+        X_leye = self.eye_conv4_2_norm(X_leye)
+
+        X_leye = F.relu(self.eye_fc1(X_leye.view(-1)))
+
+        X_reye = F.relu(self.eye_conv1_1(X_reye))
+        X_reye = F.relu(self.eye_conv1_2(X_reye))
         X_reye = self.max_pool(X_reye)
 
-        X_reye = F.relu(self.reye_conv2_1(X_reye))
-        X_reye = F.relu(self.reye_conv2_2(X_reye))
-        X_reye = F.relu(self.reye_conv2_3(X_reye))
-        X_reye = F.relu(self.reye_conv3_1(X_reye))
-        X_reye = F.relu(self.reye_conv3_2(X_reye))
-        X_reye = F.relu(self.reye_conv4_1(X_reye))
-        X_reye = F.relu(self.reye_conv4_2(X_reye))
+        X_reye = F.relu(self.eye_conv2_1(X_reye))
+        X_reye = F.relu(self.eye_conv2_2(X_reye))
 
-        X_reye = F.relu(self.reye_fc1(X_reye.view(-1)))
+        X_reye = F.relu(self.eye_conv2_3(X_reye))
+        X_reye = self.eye_conv2_3_norm(X_reye)
+
+        X_reye = F.relu(self.eye_conv3_1(X_reye))
+        X_reye = self.eye_conv3_1_norm(X_reye)
+
+        X_reye = F.relu(self.eye_conv3_2(X_reye))
+        X_reye = self.eye_conv3_2_norm(X_reye)
+
+        X_reye = F.relu(self.eye_conv4_1(X_reye))
+        X_reye = self.eye_conv4_1_norm(X_reye)
+
+        X_reye = F.relu(self.eye_conv4_2(X_reye))
+        X_reye = self.eye_conv4_2_norm(X_reye)
+
+        X_reye = F.relu(self.eye_fc1(X_reye.view(-1)))
 
         X_combined = torch.cat((X_face, X_leye, X_reye), 0)
+
+        X_combined = self.dropout(X_combined)
         X_combined = F.relu(self.combined_fc1(X_combined))
+        X_combined = self.dropout(X_combined)
+
         X_combined = F.relu(self.combined_fc2(X_combined))
 
         return X_combined
-
-
-
